d8f4822818a16dbfea2dc62e222bf2a6978ab965
Võ Hữu Thắng <huuthang28.work@gmail.com>
2022-09-22 09:03:05 +0700

    thame admin and create Categories module

---
+++ resources/views/admin/partials/siderbar.blade.php
@@ -0,0 +1,42 @@
+<div class="navbar nav_title" style="border: 0;">
+    <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Admin Manager</span></a>
+</div>
+
+<div class="clearfix"></div>
+
+<!-- menu profile quick info -->
+<div class="profile clearfix">
+    <div class="profile_pic">
+        <img src="{{ asset('admin_gentelella/production/images/img.jpg') }}" alt="..." class="img-circle profile_img">
+    </div>
+    <div class="profile_info">
+        <span>Welcome,</span>
+        <h2>John Doe</h2>
+    </div>
+</div>
+<!-- /menu profile quick info -->
+
+<br />
+
+<!-- sidebar menu -->
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+    <div class="menu_section">
+        <h3></h3>
+        <ul class="nav side-menu">
+            <li>
+                <a> {{-- href="{{route('')}}" class="nav-link" --}}
+                    <i class="fa fa-home">
+                    </i> Home </span>
+                </a>
+            </li>
+            <li><a><i class="fa fa-home"></i> Posts </span></a>   
+            </li>
+            <li><a><i class="fa fa-home"></i> Categories </span></a>   
+            </li>
+        
+        </ul>
+    </div>
+    
+
+</div>
+<!-- /sidebar menu -->
